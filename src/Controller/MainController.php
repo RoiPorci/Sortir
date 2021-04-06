96dec3ba679ba81f3b3cb299c948d821156aee6a
RoiPorci <valerian.charron@gmail.com>
2021-04-06 14:25:14 +0200

    pages inutiles

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -17,4 +17,34 @@ class MainController extends AbstractController
 
         ]);
     }
+
+    /**
+     * @Route("/about-us", name="main_about_us")
+     */
+    public function aboutUs(): Response
+    {
+        return $this->render('main/about-us.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/cgu", name="main_cgu")
+     */
+    public function cgu(): Response
+    {
+        return $this->render('main/cgu.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/legal-notice", name="main_legal_notice")
+     */
+    public function legalNotice(): Response
+    {
+        return $this->render('main/legal-notice.html.twig', [
+
+        ]);
+    }
 }
