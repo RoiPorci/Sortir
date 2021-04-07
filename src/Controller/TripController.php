d058f1adf9671cb4df489d7c2b94bad95a6bf311
Alexandrine-L <alexandrine.luseau2020@campus-eni.fr>
2021-04-07 12:15:12 +0200

    tripcontroller

---
+++ src/Controller/TripController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TripRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TripController extends AbstractController
+{
+    /**
+     * @Route("/trip/{id}", name="trip_getDetail")
+     */
+    public function getDetail(Integer $id, TripRepository $tripRepository): Response
+    {
+        $tripDetail = $tripRepository->find($id);
+
+        return $this->render('trip/detail.html.twig', [
+            'controller_name' => 'TripController',
+        ]);
+    }
+}
