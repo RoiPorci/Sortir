d973d86f6b37d961f37a64279d048f3f2800bf54
anaelle97 <anaellecloarec97@gmail.com>
2021-04-13 17:08:06 +0200

    Fonction annulation d'une sortie + css

--- src/Controller/TripController.php
+++ src/Controller/TripController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Trip;
 use App\Entity\User;
+use App\Form\CancelTripType;
 use App\Form\TripType;
 use App\Repository\CampusRepository;
 use App\Repository\StateRepository;
@@ -33,13 +34,13 @@ class TripController extends AbstractController
     }
 
     /**
-     * @Route("/trip/{id}", name="trip_getDetail")
+     * @Route("/trip/{id}", name="trip_getDetail", requirements={"id"="\d+"})
      * @param $id
      * @param TripRepository $tripRepository
      * @return Response
      * @throws NonUniqueResultException
      */
-    public function getDetail(int $id, TripRepository $tripRepository): Response
+    public function getDetail($id, TripRepository $tripRepository): Response
     {
         $tripDetail = $tripRepository->findATrip($id);
 
@@ -54,18 +55,19 @@ class TripController extends AbstractController
      * @param EntityManagerInterface $entityManager
      * @return Response
      */
-    public function create(Request $request, EntityManagerInterface $entityManager): Response
-    {
+    public function create(Request $request, EntityManagerInterface $entityManager): Response {
         $trip = new Trip();
         $form = $this->createForm(TripType::class, $trip);
 
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        if($form->isSubmitted() && $form->isValid()){
             $button = $form->getClickedButton()->getName();
-            if ($button == 'create') {
+            if ($button == 'create'){
                 $state = $this->states['created'];
-            } else {
+            }
+            else
+            {
                 $state = $this->states['opened'];
             }
 
@@ -90,22 +92,115 @@ class TripController extends AbstractController
     }
 
     /**
-     * @Route("/modify/{id}", name="trip_modify")
+     * @Route("/register/trip/{id}", name="trip_registerForATrip", requirements={"id"="\d+"})
+     */
+    public function registerForATrip($id,
+                                     TripRepository $tripRepository,
+                                     EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+
+        $tripForRegistration = $tripRepository->findATripForRegister($id);
+
+        $tripState = $tripForRegistration->getState();
+        $tripMaxRegistrationNumber = $tripForRegistration->getMaxRegistrationNumber();
+        $tripParticipants = $tripForRegistration->getParticipants()->toArray();
+
+        if (in_array($user, $tripParticipants))
+        {
+            $this->addFlash('danger', 'Vous êtes déjà inscrit!');
+            $userIsRegisteredForTrip = true;
+        } else {
+            $userIsRegisteredForTrip = false;
+        }
+
+        if (!$tripState === $this->states['opened'])
+        {
+            $this->addFlash('danger', 'Désolé, l\'inscription n\'est pas encore ouverte');
+            $tripIsOpened = false;
+        } else {
+            $tripIsOpened = true;
+        }
+
+        if (count($tripParticipants) >= $tripMaxRegistrationNumber)
+        {
+            $this->addFlash('danger', 'Désolé, la sortie est déjà complète');
+            $tripIsFull = true;
+        } else {
+            $tripIsFull = false;
+        }
+
+        if ( !$userIsRegisteredForTrip AND $tripIsOpened AND !$tripIsFull)
+        {
+            $tripForRegistration->addParticipant($user);
+            $entityManager->persist($tripForRegistration);
+            $entityManager->flush();
+            $this->addFlash('success', 'Vous êtes inscrit! ');
+        }
+
+        $tripForRegistration = $tripRepository->findATripForRegister($id);
+        $tripParticipants = $tripForRegistration->getParticipants()->toArray();
+        if (count($tripParticipants) == $tripMaxRegistrationNumber)
+        {
+            dd(count($tripParticipants));
+        }
+        //TODO clôturer la sortie si nb max atteint
+
+        return $this->redirectToRoute('main_home');
+    }
+
+    /**
+     * @Route("trip/cancel/{id}", name="trip_cancel", requirements={"id"="\d+"})
+     * @param $id
      * @param Request $request
      * @param EntityManagerInterface $entityManager
      * @return Response
      */
-    public function modify(int $id, Request $request, TripRepository $tripRepository, EntityManagerInterface $entityManager): Response
-    {
 
-        $trip = $tripRepository->findTripWithoutParticipants($id);
-        $form = $this->createForm(TripType::class, $trip);
-        //dd($trip);
+    public function cancelTrip(int $id, Request $request, TripRepository $tripRepository, StateRepository $stateRepository, EntityManagerInterface $entityManager) {
+
+        /** @var User $user */
+        $user = $this->getUser();
+        $trip = $tripRepository->findATrip($id);
+
+        if (!$trip) {
+            $this->addFlash('warning', "Cette sortie n'existe pas !");
+            return $this->redirectToRoute('main_home' );
+        }
+
+        $form = $this->createForm(CancelTripType::class, $trip);
+        $tripWording = $trip->getState()->getWording();
+        $now = new \DateTime();
+        $oldDetailTrip = $trip->getDetails();
+
+        if ($trip->getOrganiser()->getUsername() !== $user->getUsername()
+            || $trip->getDateTimeStart() < $now
+            || ($tripWording == "Activité en cours" || $tripWording == "Passée" || $tripWording == "Annulée")) {
+
+            $this->addFlash('warning', 'Vous ne pouvez pas annuler cette sortie !');
+            return $this->redirectToRoute('trip_getDetail', ['id' => $trip->getId()]);
+        }
 
         $form->handleRequest($request);
 
-        return $this->render('trip/modifyTrip.html.twig', [
-            'tripForm' => $form->createView(),
+        if ($form->isSubmitted() && $form->isValid()) {
+            $cancelDetailTrip = $form['details']->getData();
+            $text_cancel = $oldDetailTrip ." Motif d'annulation : " .$cancelDetailTrip;
+            $trip->setDetails($text_cancel);
+
+            $state = $stateRepository->findBy(['wording'=> 'Annulée'])[0];
+            $trip->setState($state);
+
+            $entityManager->persist($trip);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre sortie a été annulée');
+            return $this->redirectToRoute('trip_getDetail', ['id' => $trip->getId()]);
+        }
+
+        return $this->render('trip/cancelTrip.html.twig', [
+            'trip' => $trip,
+            'formCancelTrip' => $form->createView()
         ]);
     }
 }
