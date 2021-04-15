ae9199ef894e71131eabc9825e8a511fb872386e
RoiPorci <valerian.charron@gmail.com>
2021-04-15 09:38:41 +0200

    contraintes motif d'annulation

--- src/Form/CancelTripType.php
+++ src/Form/CancelTripType.php
@@ -8,18 +8,31 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Blank;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class CancelTripType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-
             ->add('details', TextareaType::class, [
                 'label' => 'Motif :',
                 'required' => false,
                 'mapped' => false,
-
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Veuillez renseigner un motif d'annulation!"
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => '{{ limit }} caractères minimum requis',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 200,
+                        'maxMessage' => "{{ limit }} caractères maximum"
+                    ]),
+                ],
             ])
             ->add("confirm", SubmitType::class, [
                 'label' => "Confirmer l'annulation"
